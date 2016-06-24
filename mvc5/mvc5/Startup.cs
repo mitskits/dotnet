75d9c11ccea4513bea5f0ad70b0be03406ad923f
unknown <dimitrios.koubaroulis@covata.com>
2016-06-24 23:21:40 +1000

    Initial MVC app

---
+++ mvc5/mvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc5.Startup))]
+namespace mvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
